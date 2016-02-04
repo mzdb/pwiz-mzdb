16d8647e9389404b27ff206bb28811dda650f444
AlexandreBurel <AlexandreBurel@users.noreply.github.com>
2016-02-04 16:53:03 +0100

    Moved function checkCycleNumber to file mzUtils.hpp

--- pwiz_mzdb/mzdb/writer/swath_producer.hpp
+++ pwiz_mzdb/mzdb/writer/swath_producer.hpp
@@ -9,6 +9,8 @@
 #include "cycle_obj.hpp"
 #include "spectrumlist_policy.h"
 
+#include "../../utils/mzUtils.hpp"
+
 using namespace std;
 
 namespace mzdb {
@@ -41,31 +43,6 @@ class mzSwathProducer: QueueingPolicy, MetadataExtractionPolicy {
 private:
 
     PeakPickerPolicy m_peakPicker;
-
-    /**
-     * @brief _checkCycleNumber
-     * checks if cycle number matches the "real" cycle number given in the spectrum title
-     *
-     * @param filetype origin file type (titles are differents for each file type)
-     * @param spectrumTitle in which the good cycle number may be found
-     * @param cycleNumber, the computed cycle number that may be wrong
-     */
-    void _checkCycleNumber(pwiz::msdata::CVID filetype, string spectrumTitle, int& cycleNumber) {
-        // add a special case for ABSciex files
-        if(filetype == pwiz::msdata::MS_ABI_WIFF_format) {
-            std::regex e ("cycle=(\\d+)");
-            std::smatch match;
-            if (std::regex_search(spectrumTitle, match, e) && match.size() > 0) {
-                // real cycle extracted from title
-                // if the spray gets lost, the spectra will not be written in the wiff file and the cycles wont be a list from 1 to the end
-                int cycle = std::stoi(match.str(1));
-                if(cycle != cycleNumber) {
-                    LOG(INFO) << "ABU cycle changed from " << cycleNumber << " to " << cycle << "\n";
-                    cycleNumber = cycle;
-                }
-            }
-        }
-    }
 
     /**
      * @brief _peakPickAndPush
@@ -118,7 +95,7 @@ private:
 
             if (msLevel == 1 ) {
                 ++cycleCount;
-                this->_checkCycleNumber(filetype, spectrum->id, cycleCount);
+                PwizHelper::checkCycleNumber(filetype, spectrum->id, cycleCount);
                 currMs1 = std::make_shared<mzSpectrum<h_mz_t, h_int_t> >(scanCount, cycleCount, spectrum);
                 if (cycle) {
                     //peak picks only ms == 2
